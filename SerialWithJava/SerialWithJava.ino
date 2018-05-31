86e44f58ea2c1c479772c067f67fc8ce3ae0dd64
JayPi4c <32594337+JayPi4c@users.noreply.github.com>
2018-05-31 21:47:22 +0200

    Add files via upload

---
+++ SerialWithJava/SerialWithJava.ino
@@ -0,0 +1,44 @@
+bool state;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  state = false;
+}
+
+
+
+String s = "";
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    String msg;
+    while (Serial.available() > 0) {
+      delay(42); // das delay(42); ist wichtig, da sonst die bytes nicht in den Buffer geladen werden und dann nicht alle
+      // nacheinander ausgelesen werden können.
+      byte b = Serial.read();
+      msg += (char)b;
+      //msg += "  |available: ";
+      //msg += Serial.available();
+    }
+    Serial.println(msg);
+    s = msg;
+  }
+
+
+
+
+
+  if (s.equalsIgnoreCase("CHANGE")) {
+    state = !state;
+    s = "";
+  }
+
+
+  digitalWrite(LED_BUILTIN, state);
+
+  //  Serial.println("Hello World!");
+  //  digitalWrite(LED_BUILTIN, state = !state);
+  //  delay(1000);
+}
